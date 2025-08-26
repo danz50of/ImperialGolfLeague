10c8329a64604082ada6def0b79d308b1eef5a07
Dan Zabinski <dzabinski@peerless-av.com>
2025-08-26 12:04:44 -0500

    Updating for final DEV run

--- scripts/2025mintChampion.ts
+++ scripts/2025mintChampion.ts
@@ -1,8 +1,12 @@
 import { ethers } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import hre from "hardhat";
 
 async function main() {
-  const contractAddress = "0xd0B87C0B2CF9509ef09e73654D6E2F250B90b03c";
-  const recipient = "0xE2DC81182eEb74e5472d8244cc9b8cDD4e2a78bE"; // ✅ DZ Polygon Test
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";     //DEV Address (Will change)
+//  const contractAddress = "0xd0B87C0B2CF9509ef09e73654D6E2F250B90b03c"; // AMOY Address
+//  const recipient = "0xE2DC81182eEb74e5472d8244cc9b8cDD4e2a78bE"; // ✅ DZ Polygon Test
+  const recipient = "0xbDA5747bFD65F08deb54cb465eB87D40e51B197E";  //  Account 17 in Local 8/26/2025
   const tokenURI = "ipfs://QmSWRXi4aoSiZLb9z923x2WchCnvBYhcyDw52tC1KQ9hp6/2025ChampionMetaData.json";
 
   const trophyContract = await ethers.getContractAt("AchievementNFT", contractAddress);
@@ -10,7 +14,9 @@ async function main() {
   const receipt = await tx.wait();
 
   console.log(`✅ Trophy minted to ${recipient}`);
-  console.log(`🧾 Transaction hash: ${receipt.transactionHash}`);
+  console.log(`🧾 Transaction hash: ${receipt.hash}`);
+  console.log(`📥 Receipt status: ${receipt.status}`);
+  console.log("🧾 Full receipt:", receipt);
 }
 
 main().catch((error) => {
